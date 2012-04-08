883449380cd237e70e54e09c0b07156b545e071f
Terence Flowers <terence.flowers@ally.com>
2012-04-08 01:29:11 -0400

    got working version of clock and set up nav buttons

--- js/intro-animation.js
+++ js/intro-animation.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
             var moveBox, changewidth;
 
-            moveBox = new Array('50','-50','15','-25','10');
+            moveBox = new Array(50,-50,15,-25,10,30,-20,-10,-15,10);
 
             if (sessionStorage.counter==="NaN"||sessionStorage.counter>6){
                 sessionStorage.counter="0";
@@ -15,21 +15,34 @@ $(document).ready(function(){
                 sessionStorage.counter=Number(sessionStorage.counter)+1;
             }
 
-            $('.show-number').each(function(){
-                var a = $(this).parent().width();
-                $(this).text(a);
-            });
-
             changewidth = moveBox[sessionStorage.counter];
+
+            function getPercentage(self){
+                var a = self.width();
+                // where 300 is 50% of the section (remember 600 is 100%)
+                var b = 275+a;
+                var c = b/550*50;
+                self
+                    .find('.show-number')
+                    .css({ opacity: 0.01 })
+                    .text(Math.round(c)+"%")
+                    .fadeTo(500, 1);
+            }
 
             $('.designer_side').animate(
                 {
                    width: '+='+changewidth
-                }, 1000);
+                }, 1000, function(){
+                    var self = $(this);
+                    getPercentage(self);
+                });
             $('.developer_side').animate(
                 {
                    width: '-='+changewidth
-                }, 1000);
+                }, 1000, function(){
+                    var self = $(this);
+                    getPercentage(self);
+                });
 
             // console.log("counter: " +sessionStorage.counter+ " coordinates: " +changewidth);
 
