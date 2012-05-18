bc3e3befa7b9b3b502e8ab101cc95224cb39a5f3
Terence Flowers <terence.flowers@ally.com>
2012-05-18 08:22:19 -0400

    added javascript to control open/close of the contact section via click of the contact button

--- js/site.js
+++ js/site.js
@@ -8,32 +8,86 @@ $(document).ready(function() {
 
   var contactForm = {
 
-    var form = $("#contact form");
+    createButton: function(myForm, myNav){
+      $("<li></li>", {
+        html: '<button>contact</button>',
+        class: 'extra'
+      }).appendTo("#global ul");
 
-    createButton: function() {
-      $('<li></li>', {
-        class: 'extra',
-        html: '<button>contact</button>'
-        }).appendTo('#global ul');
+      $("#global ul li button").on(
+        "click", function(e){
+          e.preventDefault();
+
+          if(myForm.hasClass('collapsed')){
+            contactForm.showForm(myForm, myNav);
+          } else {
+            contactForm.hideForm(myForm, myNav);
+          }
+
+        }
+      );
     },
 
-    showForm: function(e) {
-      e.preventDefault();
-      contactForm.form.show();
+    showForm: function(myForm, myNav){
+
+      function activateForm() {
+        myForm
+          .find('#contact_form')
+            .fadeIn("slow")
+      }
+
+      setTimeout(activateForm,1000);
+
+      myForm
+          .animate(
+          {height: '610px'},{
+            duration: 500
+          })
+          .removeClass('collapsed');
+
+      myNav.animate(
+          {top: '610px'},{
+            duration: 1000,
+            easing: 'easeInExpo'
+          }
+      );
+
 
     },
 
-    hideForm: function() {
-     // coming soon
+    hideForm: function(myForm, myNav){
+
+      myForm
+          .find('#contact_form')
+            .hide()
+            .end()
+
+          .animate(
+          {height: '0px'},{
+            duration: 500
+
+          })
+
+          .addClass('collapsed');
+
+      myNav.animate(
+          {top: '0px'},{
+            duration: 1300,
+            easing: 'easeOutExpo'
+          }
+      );
+
+    },
+
+    init: function() {
+      var myForm = $("#contact");
+      var myNav = $("#navigation");
+
+      contactForm.createButton(myForm, myNav);
     }
+  };
 
-    init: function(){
-    contactForm.createButton();
+contactForm.init();
 
-    }
-
-  }
-
-  contactForm.init();
 
 });
